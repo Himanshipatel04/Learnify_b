b12503571f3ce5e64280319b7647271c0396c943
Himanshipatel04 <himanshipatel0409@gmail.com>
2025-06-27 14:31:56 +0530

    feat: Update cookie settings to include sameSite attribute for enhanced security

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -8,6 +8,7 @@ export const googleCallback = (req, res) => {
     res.cookie('token', token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
+         sameSite: 'None',
         maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -70,6 +71,7 @@ export const registerUser = async (req, res) => {
         // 6. Set token in cookie
         res.cookie('token', token, {
             httpOnly: true, secure: process.env.NODE_ENV === 'production',
+             sameSite: 'None',
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         });
 
@@ -126,6 +128,7 @@ export const loginUser = async (req, res) => {
 
         res.cookie('token', token, {
             httpOnly: true, secure: process.env.NODE_ENV === 'production',
+             sameSite: 'None',
             maxAge: 7 * 24 * 60 * 60 * 1000,
         });
 
